d810ad13edec2965433c051674bc1d7819303708
Gabriel Weber <gabriel.aw@aluno.ifsc.edu.br>
2023-09-03 14:07:04 -0300

    first commit

---
+++ app/Models/Ferramenta.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ferramenta extends Model
+{
+    use HasFactory;
+
+    protected $title = 'ferramentas';
+    protected $fillable = [
+        'nome',
+        'marca',
+        'tipo',
+        'fornecedor',
+        'preco'
+    ];
+}
---
+++ database/migrations/2023_08_30_194758_create_cliente_pessoas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('cpf')->nullable();
+            $table->string('cnpj')->nullable();
+            $table->string('telefone');
+            $table->string('email');
+            $table->string('endereco');
+            $table->timestamps();
+        });
+
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('pedido', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('feramenta_id')->constrained('ferramenta');
+            $table->foreignId('cliente_id')->constrained('cliente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cliente_pessoas');
+    }
+};
